1aa4bf78574f8c3e59c38a09698e25e7d2556b01
CPLASF1925 <1208362643@qq.com>
2018-07-13 14:19:32 +0800

    3

--- app/src/main/java/com/bril/test/MainActivity.java
+++ app/src/main/java/com/bril/test/MainActivity.java
@@ -4,13 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.bril.widget.LoadHelper;
+
+/**
+ * @author SY
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        LoadHelper.getInstance().showLoading(this);
 
     }
 }
